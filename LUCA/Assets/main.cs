bd1df4e7c8250d989fe046010fd7fecb8377c4b4
wint3ria <nicolas.misk@gmail.com>
2015-01-22 20:39:54 +0100

    debut de la programmation du bousin
    j'ai cree un GameObject de type prefab qui sera notre cellule, un objet
    spawn pour determiner la position de notre LUCA au debut
    une classe a ete associee au gameobject environment pour gerer la partie
    au niveau code. Considerez cette classe comme le main. Il n'y a pas
    beaucoup plus que la structure generale que j'ai commence a mettre en
    place.
    
    Je me concentre dorenavant sur l'implementation du menu des
    characteristiques :)
    
    Je vous laisse le soin de voir ce que j'ai fait, j'ai malheureusement
    ete pris de court par le temps, et ai tout laisse en plan.

---
+++ LUCA/Assets/main.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // For the moment, there's no menu so the game start at the very beggining of the program :
+        initiateGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            loadPerkMenu();
+
+        }
+
+	}
+    
+    // Use this section to put anything happening at the very begining of the game
+    // Different from "Start", which starts at the beggining of the program
+    void initiateGame()
+    {
+        
+    }
+
+
+    void loadPerkMenu()
+    {
+
+    }
+}
