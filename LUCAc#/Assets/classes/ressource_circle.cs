57a31a9cad7e7264bed5b637ba98ace9b50ea47f
Biboozz <thibaut.cens@gmail.com>
2015-01-23 19:24:52 +0100

    class : point, ressource_circle, terrain
    Creation de classe

---
+++ LUCAc#/Assets/classes/ressource_circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.classes
+{
+    class ressource_circle
+    {
+        point center; //center.x & center.y
+        int rayon; //rayon in percent (100% )
+        int ressource_type;
+        int density;
+
+        public void Generate()
+        {
+            Random rand = new Random();
+            rayon = rand.Next();
+        }
+    }
+
+}
