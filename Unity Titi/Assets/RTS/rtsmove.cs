9d09cc7e1214f571ebd7b61c6ef1f3c00f7dea57
Titiwolert <etienne.legourrierec@epita.fr>
2015-02-06 00:55:28 +0100

    Unity Titi 0.0000002
    camera qui bouge avec ZQSD et fleche directionnelle ! Mise en place
    terrain et semblant de cellule

---
+++ Unity Titi/Assets/RTS/rtsmove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rtsmove : MonoBehaviour {
+
+    public float horizontalSpeed = 40;
+    public float verticalSpeed = 40;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * vertical);
+        transform.Translate(Vector3.right * horizontal);
+    
+    }
+}
