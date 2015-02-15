f117e030d5b3e67386adf29be6678f17e4a785c8
Biboozz <thibaut.cens@gmail.com>
2015-02-15 10:51:42 +0100

    Création et semiremplissage de la classe ressources

---
+++ LUCAc#/Assets/classes/ressources.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ressources : MonoBehaviour {
+
+    protected string name;
+    protected string ID;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
