abc2b35bc2a85eacadeea957443bdb6e1c952806
Biboozz <thibaut.cens@gmail.com>
2015-02-15 10:46:22 +0100

    Début de remplissage de la classe environment
    Par contre je sait pas si les listes sont gérées par Unity donc dans le
    doute je vais les remplacer par un tableau après.

--- LUCAc#/Assets/classes/environment.cs
+++ LUCAc#/Assets/classes/environment.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Object;
 
 public class environment : MonoBehaviour {
+
+    protected int Width;
+    protected int Height;
+    public List<Ressource> Ressources = new List<Ressource>();
 
 	// Use this for initialization
 	void Start () {
