92f2a72381de2f03ce45a8eaa252c8539cf46345
Biboozz <thibaut.cens@gmail.com>
2015-03-09 13:50:49 +0100

    Déplacement de POINT et creation de 2 classes
    Creation des classes RessourceCircles et RessourcePoint.
    
    Deplacement de POINT vers living.

---
+++ LUCAc#/Assets/classes/livings/RessourceCircle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using AssemblyCSharp;
+
+public class RessourceCircle : MonoBehaviour {
+
+    private POINT center;
+    private int radius;
+    //private List<>
+    private int totalAmount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- LUCAc#/Assets/classes/livings/environment.cs
+++ LUCAc#/Assets/classes/livings/environment.cs
@@ -6,7 +6,10 @@ using System;
 using AssemblyCSharp;
 
 public class environment : MonoBehaviour {
-	
+
+	private int Heigth;
+	private int Width;
+
 	public List<Species> livings = new List<Species>();
 	public GameObject cellPrefab; //just for the test, will not be needed in the futur
 	private int coolDown = 0;
@@ -40,6 +43,11 @@ public class environment : MonoBehaviour {
 				s.update();
 			}
 		}
+	}
+
+	private int get_width()
+	{
+		return Width;
 	}
 
 	public void remove(GameObject G)
