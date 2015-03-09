6f4f5d140b3909295631291778be89b70cb8e6a7
Biboozz <thibaut.cens@gmail.com>
2015-03-09 15:04:54 +0100

    Correction des erreures de compilation

--- LUCAc#/Assets/classes/livings/RessourcePoint.cs
+++ LUCAc#/Assets/classes/livings/RessourcePoint.cs
@@ -12,9 +12,9 @@ public class RessourcePoint : Ressource {
 
     public RessourcePoint(int id, string name, RessourceCircle C) : base(id, name)
     {
-        Random rand = new Random();
-        center.x = rand.Next(C.get_center().x - C.get_radius(), C.get_center().x + C.getradius);
-        center.y = rand.Next(C.get_center().y - C.get_radius(), C.get_center().y + C.getradius);
+        System.Random rand = new System.Random();
+        center.x = rand.Next(C.get_center().x - C.get_radius(), C.get_center().x + C.get_radius());
+        center.y = rand.Next(C.get_center().y - C.get_radius(), C.get_center().y + C.get_radius());
     }
 
 	// Use this for initialization
