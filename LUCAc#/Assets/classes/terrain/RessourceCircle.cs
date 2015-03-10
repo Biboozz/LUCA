fb5b5e75261492aa0862b2caa19279f4c7add789
Biboozz <thibaut.cens@gmail.com>
2015-03-10 17:15:38 +0100

    Remplissage de certaines classes de terrain

--- LUCAc#/Assets/classes/terrain/RessourceCircle.cs
+++ LUCAc#/Assets/classes/terrain/RessourceCircle.cs
@@ -7,7 +7,32 @@ using AssemblyCSharp;
 
 public class RessourceCircle
 {
-	private POINT center;
-	private List<RessourceNode> nodes = new List<RessourceNode>();
-	private int radius;
+    private molecule Molecule;
+	private POINT Center;
+	private List<RessourceNode> Nodes = new List<RessourceNode>();
+	private int Radius;
+
+    public RessourceCircle(POINT center, int radius, molecule molecule)
+    {
+        Center = center;
+        Radius = radius;
+        Molecule = molecule;
+
+        POINT p = new POINT();
+
+        int boucle = UnityEngine.Random.Range(200,301);
+
+        for (int i = 0; i < boucle; i++)
+        {
+            do
+            {
+                p.x = UnityEngine.Random.Range(Center.x - Radius, Center.x + Radius);
+                p.y = UnityEngine.Random.Range(Center.y - Radius, Center.y + Radius);
+
+            } while (p.distance(Center) > Radius);
+
+            Nodes.Add(new RessourceNode(p,molecule));
+        }
+    }
+
 }
--- LUCAc#/Assets/classes/terrain/RessourceNode.cs
+++ LUCAc#/Assets/classes/terrain/RessourceNode.cs
@@ -8,6 +8,26 @@ using AssemblyCSharp;
 public class RessourceNode
 {
 	private molecule Molecule;
-	private POINT center;
-	private int amount;
+	private POINT Center;
+	private int Amount;
+
+    public RessourceNode(POINT center, molecule molecule)
+    {
+        Center = center;
+        this.Molecule = molecule;
+
+        Amount = UnityEngine.Random.Range(100,401); //PIF
+
+    }
+
+    public molecule get_Molecule()
+    {
+        return Molecule;
+    }
+
+    public POINT get_Center()
+    {
+        return Center;
+    }
+
 }
