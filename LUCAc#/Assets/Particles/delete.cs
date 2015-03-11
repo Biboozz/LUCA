845fe68484067c670bd5b9f2c8a9158462c22378
Biboozz <thibaut.cens@gmail.com>
2015-03-12 00:41:42 +0100

    DES PARTICULLLLEEEESSSSSS BAAAAAAAAA...
    AAAAAAAAAAAAAAAAAAAAAAAAAAA...

---
+++ LUCAc#/Assets/Particles/delete.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class delete : MonoBehaviour {
+
+	private int i = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		i++;
+		if (i % 1200 == 0) 
+		{
+			Destroy(gameObject);
+			i=0;
+		}
+	}
+}
