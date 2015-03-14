609b2a58667da8f9415d6099f5be4efc1d191b87
Titiwolert <etienne.legourrierec@epita.fr>
2015-03-14 04:06:31 +0100

    Selection Box - Done :+1:
    La sélection en dessinant une drag box fonctionne, on peut voir le noyau
    changer en jaune des cellules !
    Le seul problème actuel et l'affichage de la box, j'ai testé pas mal de
    fonction  GUI mais pas trouvé encore

---
+++ LUCAc#/Assets/classes/Interface/Unit.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+	private Individual I;
+	private Color baseColor;
+	public bool selected = false;
+
+	void Start () 
+	{
+		I = gameObject.GetComponentInParent<Individual>();
+		baseColor = I.gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material.color;
+	}
+
+	private void Update () 
+	{
+		if (GetComponent<Renderer>().isVisible && Input.GetMouseButton(0)) 
+		{
+			Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+			camPos.y = CameraOperator.InvertMouseY(camPos.y);
+			selected = CameraOperator.selection.Contains(camPos);
+		}
+
+		if (selected) 
+		{
+			I.gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material.color = new Color (1,1,0,1);
+			I.gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = new Color (1,1,0,1);
+		} 
+		else 
+		{
+			I.gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material.color = baseColor;
+			I.gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = baseColor;
+		}
+	}
+}
