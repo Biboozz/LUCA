f9ff98d7eeb165946918409c621ddb9dba8a4868
Titiwolert <etienne.legourrierec@epita.fr>
2015-04-02 14:52:00 +0200

    Suppression OnClick avance debogage click selection

--- LUCAc#/Assets/classes/Interface/Unit.cs
+++ LUCAc#/Assets/classes/Interface/Unit.cs
@@ -6,6 +6,7 @@ public class Unit : MonoBehaviour {
 	private Individual I;
 	private Color baseColor;
 	public bool selected = false;
+	private bool alreadySelect = false;
 
 	void Start () 
 	{
@@ -22,22 +23,35 @@ public class Unit : MonoBehaviour {
 			selected = CameraOperator.selection.Contains(camPos);
 		}
 
-		if (Input.GetMouseButtonDown (0)) 
-		{
-			I.isSelectioned = true; 
-		}
-		else
-		{
-			I.isSelectioned = false;
-		}
-
-		if (selected) 
+		if (selected || alreadySelect) 
 		{
 			I.isSelectioned = true;
-		} 
+		}
 		else 
 		{
-			I.isSelectioned = false;
+			I.isSelectioned = false; //a REVOIR - Pose probleme !
+		}
+	}
+
+	void OnMouseDown()
+	{
+		if (I.isPlayed) 
+		{
+			I.isSelectioned = !I.isSelectioned;
+			if (I.isSelectioned) 
+			{
+				I.gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material.color = new Color (1,1,0,1);
+				I.gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = new Color (1,1,0,1);
+				alreadySelect = true;
+				selected = true;
+			}
+			else if(alreadySelect)
+			{
+				I.gameObject.transform.GetChild(2).GetComponent<MeshRenderer>().material.color = baseColor;
+				I.gameObject.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = baseColor;
+				alreadySelect = false;
+				selected = false;
+			}
 		}
 	}
 }
