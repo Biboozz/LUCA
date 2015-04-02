558e890c895eb2a5d67d6f4b80b3701d3409cc2e
wint3ria <nicolas.misk@gmail.com>
2015-04-02 23:32:26 +0200

    avancement du constructeur
    je rentre dans le vif du sujet peu à peu

---
+++ LUCAc#/tree builder/tree builder/XMLClasses/actionData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree_builder.XMLClasses
+{
+    public class actionData
+    {
+        public List<moleculePack> cellMolecules;
+        public List<moleculePack> environmentMolecules;
+        int ATP;
+
+        public actionData(int ATP, List<moleculePack> cellMolecules, List<moleculePack> environmentMolecules)
+        {
+            this.ATP = ATP;
+            this.cellMolecules = cellMolecules;
+            this.environmentMolecules = environmentMolecules;
+        }
+
+        public actionData() : this(0, new List<moleculePack>(), new List<moleculePack>())
+        {
+
+        }
+    }
+}
---
+++ LUCAc#/tree builder/tree builder/skillCreationForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tree_builder.XMLClasses;
+
+namespace tree_builder
+{
+    public partial class skillCreationForm : Form
+    {
+        public Form1 parent;
+
+        public skillCreationForm()
+        {
+            InitializeComponent();
+            skill S = new skill();
+            foreach (moleculePack mp in S.workCosts.cellMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.workCosts.environmentMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.workProducts.cellMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.workProducts.environmentMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.devCosts.cellMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.devCosts.environmentMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.devProducts.cellMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+            foreach (moleculePack mp in S.devProducts.environmentMolecules)
+            {
+                listBox1.Items.Add(mp);
+            }
+        }
+
+        
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
