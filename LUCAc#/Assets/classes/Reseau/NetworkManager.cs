bc096060e728f20748b580c2797096466081ac0c
Eriovi <baptistefortin@MacBook-Pro-de-Baptiste.local>
2015-04-29 13:49:41 +0200

    network 2

--- LUCAc#/Assets/classes/Reseau/NetworkManager.cs
+++ LUCAc#/Assets/classes/Reseau/NetworkManager.cs
@@ -1,15 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : MonoBehaviour 
+{
+	string registered_game = "network_luca_game!&@&"; //nom de notre jeu sur les serveurs unity (volontairement complexe)
+	bool is_refresing = false;
+	float refresh_request_lenght = 3.0f; //temps supplémentaire : donner une impression de recherche 
+	HostData[] host_data; //tableau contenant les serveurs créés
 
-	// Use this for initialization
-	void Start () {
-	
+	/*** création serveur ***/
+
+	private void StartServer()
+	{
+		Network.InitializeServer(16, 2500, true);
+		MasterServer.RegisterHost(registered_game, "LUCA I", "Test implamentation of server code");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/*** recherche serveur ***/
+
+	public IEnumerator refresh_host_list()
+	{
+		Debug.Log ("Recherche...");
+		MasterServer.RequestHostList (registered_game);
+		float time_started = Time.time;
+		float time_end = time_started + refresh_request_lenght;
+		
+		while (Time.time < time_end) 
+		{
+			host_data = MasterServer.PollHostList ();
+			yield return new WaitForEndOfFrame ();	
+		}
+		
+		if (host_data == null || host_data.Length == 0) 
+		{
+			Debug.Log ("Aucun serveur trouvé.");
+		} 
+		else if (host_data.Length == 1) 
+		{
+			Debug.Log (host_data.Length + " a été trouvé.");
+		} 
+		else 
+		{
+			Debug.Log (host_data.Length + " ont été trouvés.");
+		}
+		
+	}
+
+	/*** initialisation du serveur ***/
+
+	void OnserverInitialized()
+	{
+		Debug.Log ("Le serveur est initialisé.");
+	}
+
+	/*** enregistrement **/
+
+	void OnMasterServerEvent(MasterServerEvent masterServerEvent)
+	{
+		if (masterServerEvent == MasterServerEvent.RegistrationSucceeded) 
+		{
+			Debug.Log ("Enregistrement réussi.");
+		}
 	}
 }
