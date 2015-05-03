1499505cad2a38391e562e9665404fe4f51db37a
Eriovi <baptistefortin@MacBook-Pro-de-Baptiste.local>
2015-05-03 17:16:17 +0200

    network 6

--- LUCAc#/Assets/classes/Network/NetworkCallBack.cs
+++ LUCAc#/Assets/classes/Network/NetworkCallBack.cs
@@ -3,5 +3,12 @@ using System.Collections;
 
 public class NetworkCallBack : Bolt.GlobalEventListener 
 {
-	
+	public override void SceneLoadLocalDone(string map) 
+	{
+		// randomize a position
+		var pos = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
+		
+		// instantiate cube
+		BoltNetwork.Instantiate(BoltPrefabs.cell_network, pos, Quaternion.identity);	
+	}
 }
