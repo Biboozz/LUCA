e651efcef285a63ef635373a9bca1585cb594ccb
Titiwolert <etienne.legourrierec@epita.fr>
2015-05-03 19:00:10 +0200

    Network Work as promised

--- LUCAc#/Assets/classes/Network/NetworkCallBack.cs
+++ LUCAc#/Assets/classes/Network/NetworkCallBack.cs
@@ -7,9 +7,9 @@ public class NetworkCallBack : Bolt.GlobalEventListener
 	public override void SceneLoadLocalDone(string map) 
 	{
 		// randomize a position
-		var pos = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
+		var pos = new Vector3(Random.Range(0, 250), 0, Random.Range(0, 250));
 		
 		// instantiate cube
-		BoltNetwork.Instantiate(BoltPrefabs.cell_network, pos, Quaternion.identity);	
+		BoltNetwork.Instantiate(BoltPrefabs.cell_network, pos, Quaternion.Euler(90,0,0));	
 	}
 }
