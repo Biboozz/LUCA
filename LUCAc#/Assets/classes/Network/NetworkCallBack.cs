a00d70678c2d8d61507b898f287d038a3e1d7ae6
Eriovi <baptistefortin@MacBook-Pro-de-Baptiste.local>
2015-05-05 12:18:10 +0200

    event connexion

--- LUCAc#/Assets/classes/Network/NetworkCallBack.cs
+++ LUCAc#/Assets/classes/Network/NetworkCallBack.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [BoltGlobalBehaviour]
 public class NetworkCallBack : Bolt.GlobalEventListener 
@@ -10,6 +11,28 @@ public class NetworkCallBack : Bolt.GlobalEventListener
 		var pos = new Vector3(Random.Range(0, 250), 0, Random.Range(0, 250));
 		
 		// instantiate cube
-		BoltNetwork.Instantiate(BoltPrefabs.cell_network, pos, Quaternion.Euler(90,0,0));	
+		BoltNetwork.Instantiate(BoltPrefabs.cell_network, pos, Quaternion.Euler(90,0,0));
+	}
+
+
+	List<string> logMessages = new List<string>();
+
+	public override void OnEvent(LogEvent evnt) 
+	{
+		logMessages.Insert(0, evnt.Message);
+	}
+
+	void OnGUI() 
+	{
+		// only display max the 5 latest log messages
+		int maxMessages = Mathf.Min(5, logMessages.Count);
+		
+		GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
+		
+		for (int i = 0; i < maxMessages; ++i) {
+			GUILayout.Label(logMessages[i]);
+		}
+		
+		GUILayout.EndArea();
 	}
 }
