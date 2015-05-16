1db203996f566351df3e0f28423426616c20b7ae
Titiwolert <etienne.legourrierec@epita.fr>
2015-05-16 18:31:05 +0200

    Avancé IA

--- LUCAc#/Assets/classes/IA/CellUnPlayed.cs
+++ LUCAc#/Assets/classes/IA/CellUnPlayed.cs
@@ -7,20 +7,35 @@ public class CellUnPlayed : MonoBehaviour {
 
 	private Individual I;
 	private List<moleculePack> cellMolecules = new List<moleculePack>();	//Liste des molecules de la cellules
-	private molecule moleculetarget;
+	private moleculePack moleculetarget;
+	private List<moleculePack> moleculesTerrain;
 
 	// Use this for initialization
 	void Start () 
 	{
 		I = gameObject.GetComponentInParent<Individual>();
 		cellMolecules = I.cellMolecules;
-
+		//moleculesTerrain
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+
 		Debug.Log (cellMolecules [0].moleculeType);	//Acces nom de la molecule
 		Debug.Log (cellMolecules [0].count);	// Acces montant de la molecule
+	}
+
+	public string FindMoleculeLack()
+	{
+		foreach (moleculePack mp in cellMolecules) 
+		{
+			if(mp.count < moleculetarget.count)		//Si le montant de la molécule est inférieur à la molécule cible, alors cette molécule deviens la molécule cible
+			{
+				moleculetarget = mp;
+			}
+		}
+
+
 	}
 }
