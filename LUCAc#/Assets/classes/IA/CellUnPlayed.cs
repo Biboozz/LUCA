7eb9e0f029b42a2cf9410c81510363fde263b37b
Titiwolert <etienne.legourrierec@epita.fr>
2015-05-19 01:09:50 +0200

    IA

--- LUCAc#/Assets/classes/IA/CellUnPlayed.cs
+++ LUCAc#/Assets/classes/IA/CellUnPlayed.cs
@@ -40,8 +40,64 @@ public class CellUnPlayed : MonoBehaviour {
 
 	public void closeMolecule()
 	{
-		//List<moleculePack>[,] carrecell = R.moleculeRepartition[(int)(transform.position.x / 20),(int)(transform.position.y / 20)];			//Liste des molecules presentes dans la case de la cellule
+		int posx = (int)(transform.position.x / 20);
+		int posy = (int)(transform.position.y / 20);
 
+		/*for (int i = 0; i < 3; i++) //Ligne n°1
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 1 + i, posy + 5]);
+		}
+		for (int i = 0; i < 5; i++) //Ligne n°2
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 2 + i, posy + 4]);
+		}
+		for (int i = 0; i < 7; i++) //Ligne n°3
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 3 + i, posy + 3]);
+		}
+		for (int i = 0; i < 9; i++) //Ligne n°4
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 4 + i, posy + 2]);
+		}
+		for (int i = 0; i < 11; i++) //Ligne n°5
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 5 + i, posy + 1]);
+		}
+		for (int i = 0; i < 11; i++) //Ligne n°6
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 5 + i, posy]);
+		}
+		for (int i = 0; i < 11; i++) //Ligne n°7
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 5 + i, posy - 1]);
+		}
+		for (int i = 0; i < 9; i++) //Ligne n°8
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 4 + i, posy - 2]);
+		}
+		for (int i = 0; i < 7; i++) //Ligne n°9
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 3 + i, posy - 3]);
+		}
+		for (int i = 0; i < 5; i++) //Ligne n°10
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 2 + i, posy - 4]);
+		}
+		for (int i = 0; i < 3; i++) //Ligne n°11
+		{
+			moleculesAroundCell.Add(R.moleculeRepartition[posx - 1 + i, posy - 5]);
+		}
+
+		foreach (List<moleculePack>[,] L in moleculesAroundCell) 
+		{
+			foreach(moleculePack M in L)
+			{
+				if(M.moleculeType.name == FindMoleculeLack())	//Molécule trouvé
+				{
+
+				}
+			}
+		}*/
 
 	}
 }
