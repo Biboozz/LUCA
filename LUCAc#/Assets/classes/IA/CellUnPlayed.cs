c377421d56fbb6ab8aae386caa3454d9f47522c4
Titiwolert <etienne.legourrierec@epita.fr>
2015-05-19 13:50:19 +0200

    IA

--- LUCAc#/Assets/classes/IA/CellUnPlayed.cs
+++ LUCAc#/Assets/classes/IA/CellUnPlayed.cs
@@ -10,7 +10,7 @@ public class CellUnPlayed : MonoBehaviour {
 	private List<moleculePack> cellMolecules = new List<moleculePack>();	//Liste des molecules de la cellules
 	private moleculePack moleculetarget;
 	private List<moleculePack>[,] moleculesTerrain;
-	private List<List<moleculePack>[,]> moleculesAroundCell;
+	private List<List<moleculePack>> moleculesAroundCell;
 
 	// Use this for initialization
 	void Start () 
@@ -38,66 +38,95 @@ public class CellUnPlayed : MonoBehaviour {
 		return moleculetarget.moleculeType.name;
 	}
 
-	public void closeMolecule()
+	public void AnalyseMolecules()
 	{
 		int posx = (int)(transform.position.x / 20);
 		int posy = (int)(transform.position.y / 20);
+		int posx_square;
+		int posy_square;
+		bool b = true;
 
-		/*for (int i = 0; i < 3; i++) //Ligne n°1
+		for (int i = 0; i < 3 && b; i++) //Ligne n°1
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 1 + i, posy + 5]);
+			posx_square = posx - 1 + i;
+			posy_square = posy + 5;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 5; i++) //Ligne n°2
+		for (int i = 0; i < 5 && b; i++) //Ligne n°2
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 2 + i, posy + 4]);
+			posx_square = posx - 2 + i;
+			posy_square = posy + 4;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 7; i++) //Ligne n°3
+		for (int i = 0; i < 7 && b; i++) //Ligne n°3
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 3 + i, posy + 3]);
+			posx_square = posx - 3 + i;
+			posy_square = posy + 3;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 9; i++) //Ligne n°4
+		for (int i = 0; i < 9 && b; i++) //Ligne n°4
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 4 + i, posy + 2]);
+			posx_square = posx - 4 + i;
+			posy_square = posy + 2;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 11; i++) //Ligne n°5
+		for (int i = 0; i < 11 && b; i++) //Ligne n°5
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 5 + i, posy + 1]);
+			posx_square = posx - 5 + i;
+			posy_square = posy + 1;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 11; i++) //Ligne n°6
+		for (int i = 0; i < 11 && b; i++) //Ligne n°6
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 5 + i, posy]);
+			posx_square = posx - 5 + i;
+			posy_square = posy;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 11; i++) //Ligne n°7
+		for (int i = 0; i < 11 && b; i++) //Ligne n°7
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 5 + i, posy - 1]);
+			posx_square = posx - 5 + i;
+			posy_square = posy - 1;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 9; i++) //Ligne n°8
+		for (int i = 0; i < 9 && b; i++) //Ligne n°8
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 4 + i, posy - 2]);
+			posx_square = posx - 4 + i;
+			posy_square = posy - 2;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 7; i++) //Ligne n°9
+		for (int i = 0; i < 7 && b; i++) //Ligne n°9
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 3 + i, posy - 3]);
+			posx_square = posx - 3 + i;
+			posy_square = posy - 3;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 5; i++) //Ligne n°10
+		for (int i = 0; i < 5 && b; i++) //Ligne n°10
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 2 + i, posy - 4]);
+			posx_square = posx - 2 + i;
+			posy_square = posy - 4;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-		for (int i = 0; i < 3; i++) //Ligne n°11
+		for (int i = 0; i < 3 && b; i++) //Ligne n°11
 		{
-			moleculesAroundCell.Add(R.moleculeRepartition[posx - 1 + i, posy - 5]);
+			posx_square = posx - 1 + i;
+			posy_square = posy - 5;
+			b = !TestAnalyse(R.moleculeRepartition[posx_square, posy_square]);
 		}
-
-		foreach (List<moleculePack>[,] L in moleculesAroundCell) 
-		{
-			foreach(moleculePack M in L)
-			{
-				if(M.moleculeType.name == FindMoleculeLack())	//Molécule trouvé
-				{
-
-				}
-			}
-		}*/
 
 	}
+
+	public bool TestAnalyse(List<moleculePack> M)
+	{
+		foreach(moleculePack P in M)
+		{
+			if(P.moleculeType.name == FindMoleculeLack() && P.count > 0)	//Molécule trouvé
+			{
+
+				return true;
+			}
+			else
+				return false;
+		}
+	}
+
 }
