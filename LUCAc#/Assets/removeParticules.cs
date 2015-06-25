9889a0550d66bd5538b60582e214c39e247b310c
wint3ria <nicolas.misk@gmail.com>
2015-06-26 01:34:44 +0200

    des particules parce que je me faisais chier.

---
+++ LUCAc#/Assets/removeParticules.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class removeParticules : MonoBehaviour {
+
+	int duration = 2000;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		duration--;
+		if (duration == 0) {
+			Destroy(gameObject);
+		}
+	}
+}
