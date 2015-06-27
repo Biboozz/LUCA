b622913cccb7e015c31a24d34399bdcfa24bc4bd
Titiwolert <etienne.legourrierec@epita.fr>
2015-06-27 07:06:11 +0200

    Quasi fin IA espèce
    Découverte d'un beug, en effet la liste living dans environment possède
    les espèces en doublons !
    Autres choses l'espèce débloque un skill quelle n'a pas déjà et quelle
    peut débloqué, mais n'exclus pas la possibilité quelle n'est pas
    débloqué les élément précédent celui quelle va débloquer (TODO)

--- LUCAc#/Assets/classes/IA/SpeciesSkill.cs
+++ LUCAc#/Assets/classes/IA/SpeciesSkill.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using AssemblyCSharp;
 using UnityEngine.UI;
+using System.Linq;
 
 public class SpeciesSkill : MonoBehaviour {
 
@@ -13,8 +14,8 @@ public class SpeciesSkill : MonoBehaviour {
 	private int delay;
 	public System.Random rnd = new System.Random ();
 
-	private List<skill> allskill = new List<skill>{};
 	private List<skill> unlockableSkill = new List<skill>{};
+	private List<Species> SpeciesUnPlayed = new List<Species>{};
 
 	// Use this for initialization
 	void Start () 
@@ -34,9 +35,16 @@ public class SpeciesSkill : MonoBehaviour {
 
 	public void TimedUpdate ()
 	{
-		List<Species> B = Environment.livings;	//Récupération de la liste des espèces
+		foreach(Species H in Environment.livings)	//Récupération de la liste des espèces
+		{
+			SpeciesUnPlayed.Add(H);
+		}	
+
+		SpeciesUnPlayed = SpeciesUnPlayed.Distinct().ToList<Species>();	//Supprime les doublons - A regler les doublons dans Environment.livings
+
 		List<Species> R = new List<Species>{};	//Liste des espèces à supprimer
-		foreach (Species especes in B)
+
+		foreach (Species especes in SpeciesUnPlayed)
 		{
 			if (especes.isPlayed)	//Si l'espèce est une espèce joué alors on la supprime de la liste
 			{
@@ -45,39 +53,27 @@ public class SpeciesSkill : MonoBehaviour {
 		}
 		foreach (Species S in R) //Retirer les espèces jouées
 		{
-			B.Remove(S);
+			SpeciesUnPlayed.Remove(S);
 		}
-		PickupSkill (B);
-	}
 
-	public void PickupSkill(List<Species> species)
-	{
-		for (int i = 0; i < species.Count; i++) 
+		foreach (Species O in SpeciesUnPlayed)	//Pour chaque espèce non joué 
 		{
-			SpeciesUnlock(species[i]);
-		}
-	}
-
-	public void SpeciesUnlock(Species A)
-	{
-		foreach(skill P in PerkTree.perkTree)
-		{
-			allskill.Add(P);
-		}
-		
-		foreach(skill F in A.unlockedPerks)
-		{
-			if(PerkTree.isUnlockable(F, A))	//Si la compétence est débloquable
+			foreach(skill M in PerkTree.perkTree)	//Pour chaque skill existant
 			{
-				unlockableSkill.Add(F);	//Ajout le skill a la liste finale
+				if((O.unlockedPerks.Find(G => G.name == M.name)) == null && (PerkTree.isUnlockable(M, O)))	//Si le skill n'est pas déjà présent dans les skills débloqués et qu'il est débloquable
+				{
+					unlockableSkill.Add(M);	//Ajout le skill a la liste finale
+				}
 			}
-		}
-		
-		if(unlockableSkill.Count > 0)	//Si liste pas vide, sinon rien
-		{
-			int RandomSkill = rnd.Next(0, unlockableSkill.Count);	//Choisis un skill aléatoirement dans ceux débloquable
 			
-			A.naturalUnlock(unlockableSkill[RandomSkill]);	//Le débloque
+			if(unlockableSkill.Count > 0)	//Si liste pas vide, sinon rien
+			{
+				int RandomSkill = rnd.Next(0, unlockableSkill.Count);	//Choisis un skill aléatoirement dans ceux débloquable
+				
+				O.naturalUnlock(unlockableSkill[RandomSkill]);	//Le débloque
+
+				Debug.Log("L'espèce " + O.name + " a débloqué la compétence " + unlockableSkill[RandomSkill].name);
+			}
 		}
 	}
 }
